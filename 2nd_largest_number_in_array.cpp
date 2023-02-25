800b0698a94664438fbf2b29276c2642e296314e
MobeenFaryad98 <114390808+mobeenfaryad@users.noreply.github.com>
2023-02-26 01:06:33 +0500

    Create 2nd_largest_number_in_array.cpp
    To get 2nd largest Number from an Array

---
+++ 2nd_largest_number_in_array.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+   
+   int array[10] = {1,2,3,4,5,6,7,8,9,-1};
+   int largest = array[0];
+   int largest_2nd=array[0];
+   
+   
+   for( int a=1;a<10;a++)
+   {
+       
+       if(array[a]>largest)
+       {
+           largest_2nd= largest;
+           largest=array[a];
+       }
+   }
+
+
+   cout<<"2nd largest is "<< largest_2nd;
+    return 0;
+}
