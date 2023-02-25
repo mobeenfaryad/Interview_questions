652d1bef6518d629b8f7440bd9bb9a00f11266d7
MobeenFaryad98 <114390808+mobeenfaryad@users.noreply.github.com>
2023-02-26 01:31:27 +0500

    completed

---
+++ without_conditional_statement_return_100when101_inputand_vice-versa.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< endl<<"enter a number 100 or 101 : ";
+    int n ;
+    cin>>n ;
+    //100 and 101
+    
+int k = (100*101)/n; // another ways is 201-n
+cout<<"\n Number is :"<< k;
+    
+
+    return 0;
+}
